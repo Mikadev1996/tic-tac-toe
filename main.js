bfa9406564a120e0a0216a5321fffba90e853306
MikaDev1996 <charmikadev@gmail.com>
2021-09-01 21:57:19 +0100

    options menu

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-const boxes = Array.from(document.querySelectorAll(".box"));
-const endButton = document.getElementById("end");
 
-const Player = (name, shape) => {
-    shape = "";
-    name = "";
+
+
+
+const testPlayer = (name, shape) => {
+    return {name, shape}
 }
 
+const player1 = testPlayer("Mika", "X");
+const player2 = testPlayer("Dev", "O");
 
-let winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [0, 4, 8]
-];
+console.log(player1, player2);
+
+
 
 (function inputShape() {
     let gridInputs = [
@@ -25,12 +20,15 @@ let winConditions = [
         "","",""
     ];
 
+
+    const endButton = document.getElementById("end");
     endButton.addEventListener("click", () => {
         gameIsOver(gridInputs);
     });
 
     let state = true;
 
+    const boxes = Array.from(document.querySelectorAll(".box"));
     boxes.forEach((box) =>{
         box.addEventListener("click", () => {
             box.textContent = state ? "X" : "O";
@@ -42,16 +40,39 @@ let winConditions = [
     })
 
 
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [0, 4, 8]
+    ];
+
+    const gameIsOver = (arr, shape) => {
+        winConditions.forEach((set) => {
+            if (arr[set[0]] === shape && arr[set[1]] === shape && arr[set[2]] === shape) {
+                alert(`Congratulations ${shape} has won!`)
+
+                gridInputs = [
+                    "","","",
+                    "","","",
+                    "","",""
+                ];
+
+                boxes.forEach((box) => {
+                    box.textContent = "";
+                })
+
+            }
+        })
+
+    }
 })();
 
-function gameIsOver(arr, shape) {
-    winConditions.forEach((set) => {
-        if (arr[set[0]] === shape && arr[set[1]] === shape && arr[set[2]] === shape) {
-            alert(`Congratulations ${shape} has won!`)
-        }
-    })
 
-}
 
 
 
